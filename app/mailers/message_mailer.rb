cc0e264ded8189a3fee029133e0d66722be840d0
Johan Sveholm <jsveholm@gmail.com>
2011-02-16 13:59:03 +0900

    cleaned up the mail message

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,7 +1,12 @@
 class MessageMailer < ActionMailer::Base
   #default :from => "someone@out.there"
 
-  def message_confirmation
-    mail(:to => "jsveholm@gmail.com", :subject => "WoW!", :from => "someone@out.there")
+  def message_confirmation(message)
+    @namn = message.name
+    @email = message.email
+    @company = message.company
+    @phone = message.phone
+    @message = message.body
+    mail(:to => "jsveholm@gmail.com", :subject => message.subject, :from => message.email)
   end
 end
